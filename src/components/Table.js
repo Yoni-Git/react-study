f08708f99f698c6c3eef81f12a995b2133724a6e
yewbdar <yewbdar.grima@Gmail.com>
2019-03-30 12:52:10 -0500

    display data in table

---
+++ src/components/Table.js
@@ -0,0 +1,32 @@
+import  React ,{Component} from 'react'
+
+class Table extends Component{
+    getHeader(){
+
+    }
+    render(){
+        return(
+            <div>
+                <table>
+                    <tbody className='table'>
+                    {this.props.header.map((items,i) => {return(<th key={i}>{items}</th>)})}
+                    </tbody>
+                    <tbody>
+                    {this.props.user.map((data,i) =>{return(<tr>
+                        <td key={i}>{data.id}</td>
+                        <td key={i}>{data.first_name}</td>
+                        <td key={i}>{data.last_name}</td>
+                        <td key={i}>{data.avatar}</td>
+
+                    </tr>
+
+                                                            )})}
+                    </tbody>
+
+                </table>
+            </div>
+
+        )
+    }
+}
+export default Table
